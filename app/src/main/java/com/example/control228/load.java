74d082d042afc9ecb4c7bf066955391d5c46de08
Saytell <parygin1245@gmail.com>
2022-11-30 10:44:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/control228/load.java
@@ -0,0 +1,13 @@
+package com.example.control228;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class load extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
